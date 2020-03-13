227563c5f7f82775f66916a7c4712e17d8d17f2d
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-13 08:56:11 -0700

    write vender tests

--- VenderTracker.Tests/ModelTests/VenderTests.cs
+++ VenderTracker.Tests/ModelTests/VenderTests.cs
@@ -1,16 +1,73 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VenderTracker.Models;
 
 namespace VenderTracker.Tests
 {
   [TestClass]
-  public class VenderTests
+  public class VenderTests : IDisposable
   {
-    [TestMethod]
-    public void MethodName1_DescriptionOfBehavior1_ExpectedResult1()
+
+    public void Dispose()
     {
-      Vender vender = new Vender();
+      Vender.ClearAll();
+    }
+
+    [TestMethod]
+    public void VenderConstructor_CreateVender_Vender()
+    {
+      Vender vender = new Vender("name", "description");
       Assert.AreEqual(typeof(Vender), vender.GetType());
+    }
+
+    [TestMethod]
+    public void VenderConstructor_InitializeProperties_VenderProperties()
+    {
+      string name = "name";
+      string description = "description";
+      List<Order> emptyList = new List<Order>() { };
+      Vender vender = new Vender(name, description);
+      Assert.AreEqual(name, vender.Name);
+      Assert.AreEqual(description, vender.Description);
+      Assert.AreEqual(emptyList, vender.Orders);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnVenderId_Int()
+    {
+      Vender vender = new Vender("name", "description");
+      int result = vender.Id;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnAllVenders_VenderList()
+    {
+      Vender vender1 = new Vender("name1", "description1");
+      Vender vender2 = new Vender("name2", "description2");
+      List<Vender> expectedList = new List<Vender> { vender1, vender2 };
+      List<Vender> result = Vender.GetAll();
+      CollectionAssert.AreEqual(expectedList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnSpecificVender_Vender()
+    {
+      Vender vender1 = new Vender("name1", "description1");
+      Vender vender2 = new Vender("name2", "description2");
+      Vender result = Vender.Find(2);
+      Assert.AreEqual(vender2, result);
+    }
+
+    [TestMethod]
+    public void AddOrder_AddOrderToSpecificVender_OrderList()
+    {
+      Order order = new Order("title", "itemType", 2, 5.50, "dueDate");
+      List<Order> expectedOrders = new List<Order> { order };
+      Vender vender = new Vender("name", "description");
+      vender.AddOrder(order);
+      List<Order> result = vender.Orders;
+      CollectionAssert.AreEqual(expectedOrders, result);
     }
   }
 }
